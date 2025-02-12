605b3ec653da1f2810dc517cd8d3244bcff274f3
CyberTree <CyberTree@TE-NOS-D-113-01>
2025-02-12 20:01:50 +0100

    pola figur

--- IntroductionASPNETCoreMVC/Controllers/PolaFigurController.cs
+++ IntroductionASPNETCoreMVC/Controllers/PolaFigurController.cs
@@ -8,9 +8,30 @@ namespace IntroductionASPNETCoreMVC.Controllers
         {
             return View();
         }
-        public IActionResult PoleKwadratu()
+        public IActionResult PoleKwadratu(int bok)
         {
+            //jeżeli bok będzie miało wartość 0 lub mniej nie wykonujemy obliczenia
+            if (bok > 0)
+            {
+                int pole = bok * bok;
+                ViewBag.PoleKwadratu = pole;
+            }
             return View();
         }
+        public IActionResult PoleTrojkata(float bok, float wysokosc)
+        {
+            if (bok > 0 && wysokosc>0)
+            {
+                float pole = bok * wysokosc * 0.5f;
+                ViewBag.PoleTrojkata = pole;
+            }
+            return View();
+        }
+
+        //prostokąt
+        //trójkąt
+        //trapez
+        //romb
+        //koło
     }
 }
