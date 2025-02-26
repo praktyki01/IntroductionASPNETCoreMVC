3b3fb3d430618fce5daa52ba63e40ec67aab514c
CyberTree <CyberTree@TE-NOS-D-113-01>
2025-02-26 20:04:50 +0100

    javascript

---
+++ IntroductionASPNETCoreMVC/Controllers/MultipleInputsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IntroductionASPNETCoreMVC.Controllers
+{
+    public class MultipleInputsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Kalkulator()
+        {
+            return View();
+        }
+        public IActionResult KalkulatorJS()
+        {
+            return View();
+        }
+        public IActionResult PoleTrojkataJS()
+        {
+            return View();
+        }
+    }
+}
